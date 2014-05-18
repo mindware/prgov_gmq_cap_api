59bc4ffae155847afea2198f471cb5e3ef1d1f54
Andrés Colón <andres@thoughtware.tv>
2014-05-18 18:00:32 -0400

    Testing for bad username / password, and other errors. Passed.

--- scripts/go.sh
+++ scripts/go.sh
@@ -1,2 +1,11 @@
-echo "Deleting:"
-"curl -v -X DELETE http://localhost:9000/v1/cap/transaction/1"
+echo "Trying valid user."
+curl -u policia:password -X GET 'http://localhost:9000/v1/cap/transaction/1'
+echo ""
+echo ""
+echo "Trying invalid user."
+curl -u policia2:password -X GET 'http://localhost:9000/v1/cap/' -i
+echo ""
+echo ""
+echo "Trying transaction. Checking entities."
+curl -u andres:password -X GET 'http://localhost:9000/v1/cap/transaction/1' 
+echo ""
