c525eef7a5cca01ff29767cc5fd3917a48047a49
Andrés Colón <andres@thoughtware.tv>
2014-05-21 04:55:47 -0400

    Cleaning up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,19 @@ module PRGMQ
         attr_reader :name, :groups
 
         def initialize(name, groups)
-          error!(InvalidAccess.data,  InvalidAccess.http_code) if groups.class != Array
+          raise InvalidUserGroup if groups.class != Array
+          # if for some reason a user wasn't configured with a proper
+          # group, we delete whatever it is that was stored and
+          # setup the group properly. - Nevermind, we'll error out.
+          # if(groups.class != Array)
+          #    groups = []
+          # end
           @name = name
           # By default, always add the 'all' group.
           # This group doesn't need to be stored in the server, since
           # all users belong to it, always.
           @groups = groups
-          @groups.push "all"
+          @groups.push "all" unless @groups.include? "all"
         end
 
     end
