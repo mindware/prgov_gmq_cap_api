e005bfb7c509235a5a6f1cfe4a95ca9e011013a2
Andrés Colón <andres@thoughtware.tv>
2014-05-27 04:06:52 -0400

    Renaming old id_factory to transaction_id_factory

---
+++ app/models/transaction_id_factory.rb
@@ -0,0 +1,17 @@
+require 'securerandom'
+
+module PRGMQ
+  module CAP
+    class TransactionIdFactory < Toy::Identity::AbstractKeyFactory
+      # How should the id be typecast
+      def key_type
+        String
+      end
+
+      # the transaction list will always have the same name
+      def next_key(object)
+        "transactions"
+      end
+    end
+  end
+end
