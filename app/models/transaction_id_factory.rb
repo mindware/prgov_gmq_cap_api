4ee152ae265e4fe6b71d6b574122cdf284a2cde9
Andrés Colón <andres@thoughtware.tv>
2014-05-27 14:12:42 -0400

    Using SecureRandom.uuid for Transaction ID Factory instead of SimpleUUID

--- app/models/transaction_id_factory.rb
+++ app/models/transaction_id_factory.rb
@@ -3,13 +3,10 @@ require 'securerandom'
 module PRGMQ
   module CAP
     class TransactionIdFactory < Toy::Identity::AbstractKeyFactory
-      # How should the id be typecast
-      def key_type
-        String
-      end
 
       # the transaction list will always have the same name
-      def next_key(object)
+      def generate_key
+        SecureRandom.uuid
       end
     end
   end
