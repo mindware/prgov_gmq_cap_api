2e544251300827e972a702cc6cc62178dfcc0818
Andrés Colón <andres@thoughtware.tv>
2014-05-29 19:27:18 -0400

    Adding both instance and class methods to ID Factory

--- app/helpers/transaction_id_factory.rb
+++ app/helpers/transaction_id_factory.rb
@@ -2,12 +2,29 @@ require 'securerandom'
 
 module PRGMQ
   module CAP
-    module TransactionIdFactory 
+    module TransactionIdFactory
 
       # the transaction list will always have the same name
       def generate_key
+        TransactionIdFactory.generate_key
+      end
+
+      # Checks the length of our keys. We simply generate one
+      # to check. Used by the Validations helper
+      def transaction_key_length
+         TransactionIdFactory.generate_key.length
+      end
+
+      # Checks the length of our keys. We simply generate one
+      # to check. Used by the Validations helper
+      def self.transaction_key_length
+         self.generate_key.length
+      end
+
+      def self.generate_key
         SecureRandom.uuid.gsub("-", "")
       end
+
     end
   end
 end
