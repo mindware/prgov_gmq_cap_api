cfcf443c54cb563ab62bce4cf9ff672abfb3a2e8
Andrés Colón <andres@thoughtware.tv>
2014-05-30 13:49:48 -0400

    Battle testing.

--- scripts/test/anpe/review_complete.rb
+++ scripts/test/anpe/review_complete.rb
@@ -4,21 +4,8 @@ require 'base64'
 
 user = "***REMOVED***"
 pass = "***REMOVED***"
-
-url = "http://localhost:9000/v1/cap/transaction/certificate_ready"
-# payload = JSON.parse('{"first_name" : "Andrés", "last_name" : "Colón",
-#             "mother_last_name" : "Pérez", "ssn" : "111-22-3333",
-#             "license" : "12345678", "birth_date" : "01/01/1982",
-#             "residency" : "San Juan", "IP" : "192.168.1.2",
-#             "reason" : "Solicitud de Empleo", "birth_place" : "San Juan",
-#             "email" : "acolon@ogp.pr.gov", "license_number" : "1234567"}')
 id = '1e29234ee0c84921adec08fbe5980162'
-# file = File.open("./sample/sagan.jpg", "rb")
-# contents = file.read
-# cert64 = Base64.strict_encode64(contents)
-# payload = { "id" => id,
-#             "certificate_base64" => cert64 }
-
+url = "http://localhost:9000/v1/cap/transaction/review_complete"
 anpe_action_id      = "030"
 anpe_transaction_id = "anpe12345"
 approval_date       = Time.now.utc
