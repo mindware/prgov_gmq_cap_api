1a48b4c521a89bd1c5deb1e7739027db51c74971
Andrés Colón <andres@thoughtware.tv>
2014-06-03 11:15:41 -0400

    Adding support for storing analyst_fullname and analyst_id.

--- scripts/test/anpe/review_complete.rb
+++ scripts/test/anpe/review_complete.rb
@@ -6,16 +6,21 @@ user = "***REMOVED***"
 pass = "***REMOVED***"
 id = '1e29234ee0c84921adec08fbe5980162'
 url = "http://localhost:9000/v1/cap/transaction/review_complete"
-anpe_action_id      = "030"
-anpe_transaction_id = "anpe12345"
-approval_date       = Time.now.utc
-decision            = "100"
+
+analyst_internal_status_id      = "030" # the action's id in the system
+analyst_id 		                 = "123" # the id of the user in the db
+analyst_fullname	              = "Walter Lamela"
+analyst_transaction_id 		     = "analyst12345" # the id of the record in the db
+approval_date      	           = Time.now.utc
+decision           		         = "100"
 
 payload = {
               "id" => id,
               "decision_code" => decision,
-              "analyst_internal_status_id" => anpe_action_id,
-              "analyst_transaction_id" => anpe_transaction_id,
+      	      "analyst_id"    => analyst_id,
+      	      "analyst_fullname" => analyst_fullname,
+              "analyst_internal_status_id" => analyst_internal_status_id,
+              "analyst_transaction_id" => analyst_transaction_id,
               "analyst_approval_datetime" => approval_date
           }
 method = "put"
