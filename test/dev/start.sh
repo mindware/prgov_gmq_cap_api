14e3d367d420b60a002ecf95067b6539166efde5
Andrés Colón <andres@thoughtware.tv>
2014-06-29 05:14:32 -0400

    No longer needing the dev test folder. Removing it.

--- test/dev/start.sh
+++
@@ -1,4 +0,0 @@
-USER_HOME=$(eval echo ~${SUDO_USER})
-$USER_HOME/gmq/twemproxy/src/nutcracker -c $USER_HOME/gmq/twemproxy/conf/gmq.yml &
-redis-server /usr/local/etc/redis.conf & 
-redis-server /usr/local/etc/redis2.conf &
