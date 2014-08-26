a58a31ff6128cfa0cd22d44c807d4b856dc58baa
Andrés Colón <andres@thoughtware.tv>
2014-08-26 17:01:56 -0400

    Adding test class for generating multiple transactions consecutively.

---
+++ test/systems/prgov/multi_create.rb
@@ -0,0 +1,49 @@
+require '../lib/rest'
+
+def setup
+user = "***REMOVED***"
+pass = "***REMOVED***"
+# credentials via basic auth
+url = "http://localhost:9000/v1/cap/transaction/"
+first_name = 'Andrés' 
+last_name  = 'Colón'
+mother_last_name = 'Pérez' 
+ssn =	'111-22-3333'
+license = '123456789'
+birth_date = '01/01/1982' 
+residency  = 'San Juan'
+ip = '192.168.1.2'
+reason = 'Background check to join S.H.I.E.L.D.'
+birth_place = "San Juan"
+email = "acolon@ogp.pr.gov"
+# Test it in english and spanish. Comment last one to try the other.
+language = 'english'
+language = 'spanish'
+
+first_name << (rand(500) + rand(500)).to_s 
+payload = { 
+		:first_name => first_name,
+		:last_name  => last_name,
+		:mother_last_name => mother_last_name,
+		:ssn	=> ssn,
+		:license_number => license,
+		:birth_date => birth_date,
+		:residency  => residency,
+		:IP	    => ip,
+		:reason	    => reason,
+		:birth_place=> birth_place,
+		:email	    => email,
+		:language   => language
+	 }
+method = "post"
+type = "json"
+
+a = Rest.new(url, user, pass, type, payload, method)
+a.request
+end
+
+total = 10
+(1..total).each do |i|
+	setup
+end
+puts "Done! (#{total} added)"
